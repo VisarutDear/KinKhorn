9cc1b9a12185b0a76eaff344400c3e439302c47f
Visarut Techatanchai <61011358@kmitl.ac.th>
2021-04-27 01:19:56 +0700

    Update AccountPage.tsx

--- app/Frontend/src/Pages/AccountPage.tsx
+++ app/Frontend/src/Pages/AccountPage.tsx
@@ -1,9 +1,13 @@
+import { Card } from '@material-ui/core'
 import React from 'react'
 
 export default function AccountPage() {
     return (
         <div className="content-wrapper">
-            AccountPage
+            <h2>My Account</h2>
+            <Card>
+                ...
+            </Card>
         </div>
     )
 }
