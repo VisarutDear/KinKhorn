a97170a4be2e5a1dd1cb52edc5534180a2df1d47
Phirankhridth <61011312@kmitl.ac.th>
2021-05-22 03:26:34 +0700

    Update History Page

--- Frontend/src/Pages/HistoryPage.tsx
+++ Frontend/src/Pages/HistoryPage.tsx
@@ -55,7 +55,7 @@ export default function HistoryPage() {
     //   .then((res) => console.log("res :", res));
   }, []);
   return (
-    <div className={classes.root}>
+    <div style={{ width: "100%" }} className={classes.root}>
       <Paper className={classes.paper}>
         <Grid container wrap="nowrap" spacing={2}>
           <Grid item>
@@ -76,11 +76,11 @@ export default function HistoryPage() {
                 </Typography>
               </Grid>
             </Grid>
-            <Grid item>
+            {/* <Grid item>
               <Typography variant="body2" style={{ cursor: "pointer" }}>
                 Rate Order
               </Typography>
-            </Grid>
+            </Grid> */}
           </Grid>
         </Grid>
       </Paper>
@@ -90,11 +90,11 @@ export default function HistoryPage() {
             <Grid item xs container direction="column" spacing={1}>
               <Grid item xs>
                 <Typography variant="body2" gutterBottom>
-                  Where: Canteen Name
+                <i className="fas fa-map-marker-alt"/> Canteen Name
                 </Typography>
-                <Typography variant="body2" gutterBottom>
+                {/* <Typography variant="body2" gutterBottom>
                   To: HM Building
-                </Typography>
+                </Typography> */}
               </Grid>
             </Grid>
             <Grid item></Grid>
@@ -104,48 +104,95 @@ export default function HistoryPage() {
       <Paper className={classes.paper}>
       <Grid container spacing={3}>
         <Grid item xs={12}>
-        <Box fontWeight="fontWeightBold" m={0}>
+        
+        <Box fontWeight="fontWeightBold" m={-0.5}>
+        
           Order Summary
-        </Box>
-        </Grid>
-        <Grid item xs={11}>
-        <Typography variant="body2">
-              1x Spaghetti Carbonara
-              </Typography>
-              <Typography variant="body2">
-              1x Spaghetti Carbonara
-              </Typography>
+          
+          </Box>
+          
         </Grid>
         <Grid item xs={1}>
         <Typography variant="body2">
-              60
+          <Box fontWeight="fontWeightRegular" >
+              1x
+              </Box>
               </Typography>
               <Typography variant="body2">
-              60
+              <Box fontWeight="fontWeightRegular"  >
+              10x
+              </Box>
+              </Typography>
+        
+        </Grid>
+        <Grid item xs={9}>
+        <Typography variant="body2">
+          <Box fontWeight="fontWeightRegular" >
+              Spaghetti Carbonara
+              </Box>
+              </Typography>
+              <Typography variant="body2">
+              <Box fontWeight="fontWeightRegular" >
+              Spaghetti Carbonara
+              </Box>
+              </Typography>
+        </Grid>
+        <Grid item xs={2}>
+        <Typography variant="body2">
+        <Box textAlign="right">
+                60
+                </Box>
+              </Typography>
+              <Typography variant="body2">
+              <Box textAlign="right">
+                60
+                </Box>
               </Typography>
         </Grid>
       </Grid>
       </Paper>
       <Paper className={classes.paper}>
-        <Grid container wrap="nowrap" spacing={2}>
-          <Grid item xs zeroMinWidth>
-            <Grid item xs container direction="column" spacing={1}>
-              <Grid item xs>
-                <Typography variant="body2" gutterBottom>
-                  Subtotal ฿240
-                </Typography>
-                <Typography variant="body2" gutterBottom>
-                  Delivery Fee ฿10
-                </Typography>
-                <Typography gutterBottom variant="subtitle1">
-                  Total ฿250
-                </Typography>
+      <Grid container spacing={2}>
+      <Grid item xs={10}>
+        <Box fontWeight="fontWeightLight" m={-0.5}>
+          Subtotal
+        </Box>
+        <Box fontWeight="fontWeightLight" m={-0.5}>
+          Delivery Fee
+        </Box>
+        </Grid>
+              <Grid item xs={2}>
+              <Typography variant="body2">
+              <Box textAlign="right">
+                200
+                </Box>
+              </Typography>
+              <Typography variant="body2">
+              <Box textAlign="right">
+                2
+                </Box>
+              </Typography>
+              
+        </Grid>
+        <Grid item xs={10}>
+        <Typography variant="body2">
+              <Box fontWeight="fontWeightBold" m={-0.5}>
+          Total
+        </Box>
+        </Typography>
+        </Grid>
+        <Grid item xs={2}>
+              <Typography variant="body2">
+              <Box textAlign="right">
+                14000
+                </Box>
+              </Typography>
               </Grid>
               <Grid item></Grid>
             </Grid>
             <Grid item></Grid>
-          </Grid>
-        </Grid>
+          {/* </Grid> */}
+        {/* </Grid> */}
       </Paper>
     </div>
   );
